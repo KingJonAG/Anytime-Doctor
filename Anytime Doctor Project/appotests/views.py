7b8026333d07046bdbcb369579dca2d478d18b67
KingJonAG <akilagodigamuwa@gmail.com>
2021-02-07 23:02:24 +0530

    modified confirm appo

--- Anytime Doctor Project/appotests/views.py
+++ Anytime Doctor Project/appotests/views.py
@@ -1,17 +1,30 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404,redirect
 from django.http import HttpResponse
 from doctors.models import doctor
 from hospitals.models import hospital
+from .models import appointment_details
+from django.contrib.auth.models import User,auth
+from django.contrib import messages
 
-def confirmAppointment(request,doc_id):    
+def confirmAppointment(request,doc_id):  
 
-    doctor_p=get_object_or_404(doctor,pk=doc_id)
-    hospitals=hospital.objects.all()
+    if request.method== "POST":
+        date=request.POST["date"]
+        time=request.POST["time"]
 
-    context = {
-        "doctor_p":doctor_p,
-        "hospitals":hospitals
-    }
+        appointment=appointment_details(patient=User,doctor=doc_id,hospital=doctor.hospital,date=date,time=time)
+        appointment.save()
+        messages.success(request,"Appointment Placed")
+        return redirect("dashboard")
+        
+    else:
+        doctor_p=get_object_or_404(doctor,pk=doc_id)
+        hospitals=hospital.objects.all()
+
+        context = {
+            "doctor_p":doctor_p,
+            "hospitals":hospitals
+        }
 
 
     return render(request,"appotests/confirm_appointment.html", context)    
