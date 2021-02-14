95f226fb6cc6d9b82dd927eff1fd6d28880d003b
KingJonAG <akilagodigamuwa@gmail.com>
2021-02-14 22:56:44 +0530

    some changes to appotests

--- Anytime Doctor Project/appotests/views.py
+++ Anytime Doctor Project/appotests/views.py
@@ -1,21 +1,22 @@
 from django.shortcuts import render, get_object_or_404,redirect
+from django.contrib.auth.models import User
 from django.http import HttpResponse
 from doctors.models import doctor
 from hospitals.models import hospital
 from .models import appointment_details
-from django.contrib.auth.models import User,auth
 from django.contrib import messages
 
 def confirmAppointment(request,doc_id):  
 
-    if request.method== "POST":
+    if request.method == "POST":
         date=request.POST["date"]
         time=request.POST["time"]
 
-        appointment=appointment_details(patient=User,doctor=doc_id,hospital=doctor.hospital,date=date,time=time)
+
+        appointment=appointment_details(doctor=doctor.objects.get,hospital=doctor.hospital,date=date,time=time)
         appointment.save()
         messages.success(request,"Appointment Placed")
-        return redirect("dashboard")
+        return redirect("dashboard")  
         
     else:
         doctor_p=get_object_or_404(doctor,pk=doc_id)
